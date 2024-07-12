9bdc7bb9e6a16d7e99a25d1bfa69ab9caa7e2b3f
LMBarboza <leon.uchoa@gmail.com>
2024-07-12 16:06:50 -0300

    add rede base

---
+++ lista2/modules/rede.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class RedeBase(nn.Module):
+    def __init__(self, sInput: int, sOutput: int) -> None:
+        super().__init__()
+        self.sInput = sInput
+        self.sOutput = sOutput
+        self.layers = nn.ModuleList()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for layer in self.layers:
+            x = layer(x)
+        return x
